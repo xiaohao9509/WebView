12e796aaa9fbe6021b55f4b5706b3b7f65896025
Nick <xiaonick9509@163.com>
2016-07-17 13:49:23 +0800

    创建工程

---
+++ app/src/main/java/com/nick/webview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nick.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView web_View;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web_View = (WebView) findViewById(R.id.web_view);
+        web_View.loadUrl("https://www.qq.com/");
+    }
+}
